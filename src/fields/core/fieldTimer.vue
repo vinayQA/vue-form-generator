55f0ffb956bdb5ee8201f3b4ce1414c5d007ad7f
vinayQA <vinay@samitsolutions.com.com>
2018-11-21 06:22:53 +0530

    f

--- src/fields/core/fieldTimer.vue
+++ src/fields/core/fieldTimer.vue
@@ -1,87 +1,114 @@
 <template lang="pug">
-	.input-group.date
-		input.form-control(type="text", v-model="value", :required="schema.required", :autocomplete="schema.autocomplete", :disabled="disabled", :placeholder="schema.placeholder", :readonly="schema.readonly", :name="schema.inputName", :id="getFieldID(schema)")
-		span.input-group-addon
+	span(:id="getFieldID(schema)", :class="schema.fieldClasses") {{ value }}
 </template>
 
 <script>
-	 	import abstractField from "../abstractField";
-	 	import { defaults } from "lodash";
+	import abstractField from "../abstractField";
 
-		// let times= [];
-    // let animateFrame= 0;
-    //   let nowTime= 0;
-    //   let diffTime= 0;
-    //   let startTime= 0;
-    //   let isRunning= false;
-
-   export default({
-  name: "Timer",
-  data(){
-    return {
-      times: [],
-      animateFrame: 0,
-      nowTime: 0,
-      diffTime: 0,
-      startTime: 0,
-      isRunning: false
-    };
-  },
-  methods: {
-    setSubtractStartTime: function () {
-      let time = typeof time !== 'undefined' ? time : 0;
-      this.startTime = Math.floor(performance.now() - time);
-    },
-    startTimer: function () {
-      let vm = this;
-      vm.setSubtractStartTime(vm.diffTime);
-      (function loop(){
-        vm.nowTime = Math.floor(performance.now());
-        vm.diffTime = vm.nowTime - vm.startTime;
-        vm.animateFrame = requestAnimationFrame(loop);
-      }());
-      vm.isRunning = true;
-    },
-    stopTimer: function () {
-      this.isRunning = false;
-      cancelAnimationFrame(this.animateFrame);
-    },
-    pushTime: function () {
-      this.times.push({
-        hours: this.hours,
-        minutes: this.minutes,
-        seconds: this.seconds,
-        milliSeconds: this.milliSeconds
-				});
-    },
-    // clearAll: function () {
-    //   this.startTime = 0;
-    //   this.nowTime = 0;
-    //   this.diffTime = 0;
-    //   this.times = [];
-    //   this.stopTimer();
-    //   this.animateFrame = 0;
-    // }
-  },
-  computed: {
-    hours: function () {
-      return Math.floor(this.diffTime / 1000 / 60 / 60);
-    },
-    minutes: function () {
-      return Math.floor(this.diffTime / 1000 / 60) % 60;
-    },
-    seconds: function () {
-      return Math.floor(this.diffTime / 1000) % 60;
-    },
-    milliSeconds: function () {
-      return Math.floor(this.diffTime % 1000);
-    }
-  },
-  filters: {
-    zeroPad: function(value){
-      let num = typeof num !== 'undefined' ? num : 2;
-      return value.toString().padStart(num,"0");
-    }
-  }
-});
+	export default {
+		mixins: [ abstractField ]
+	};
 </script>
+
+<style lang="sass">
+	.vue-form-generator .field-label span {
+		display: block;
+		width: 100%;
+		margin-left: 12px;
+	}
+</style>
+
+<!-- //- <template lang="pug">
+//- 	.input-group.date
+//- 		input.form-control(type="text", v-model="value",
+//- 		:required="schema.required",
+//- 		:autocomplete="schema.autocomplete",
+//- 		:disabled="disabled",
+//- 		:placeholder="schema.placeholder",
+//- 		:readonly="schema.readonly",
+//- 		:name="schema.inputName",
+//- 		:id="getFieldID(schema)")
+//- 		span.input-group-addon
+//- </template>
+//-
+//- <script>
+//- 	 	import abstractField from "../abstractField";
+//- 	 	import { defaults } from "lodash";
+//-
+//- 		// let times= [];
+//-     // let animateFrame= 0;
+//-     //   let nowTime= 0;
+//-     //   let diffTime= 0;
+//-     //   let startTime= 0;
+//-     //   let isRunning= false;
+//-
+//-    export default({
+//-   name: "Timer",
+//-   data(){
+//-     return {
+//-       times: [],
+//-       animateFrame: 0,
+//-       nowTime: 0,
+//-       diffTime: 0,
+//-       startTime: 0,
+//-       isRunning: false
+//-     };
+//-   },
+//-   methods: {
+//-     setSubtractStartTime: function () {
+//-       let time = typeof time !== 'undefined' ? time : 0;
+//-       this.startTime = Math.floor(performance.now() - time);
+//-     },
+//-     startTimer: function () {
+//-       let vm = this;
+//-       vm.setSubtractStartTime(vm.diffTime);
+//-       (function loop(){
+//-         vm.nowTime = Math.floor(performance.now());
+//-         vm.diffTime = vm.nowTime - vm.startTime;
+//-         vm.animateFrame = requestAnimationFrame(loop);
+//-       }());
+//-       vm.isRunning = true;
+//-     },
+//-     stopTimer: function () {
+//-       this.isRunning = false;
+//-       cancelAnimationFrame(this.animateFrame);
+//-     },
+//-     pushTime: function () {
+//-       this.times.push({
+//-         hours: this.hours,
+//-         minutes: this.minutes,
+//-         seconds: this.seconds,
+//-         milliSeconds: this.milliSeconds
+//- 				});
+//-     },
+//-     // clearAll: function () {
+//-     //   this.startTime = 0;
+//-     //   this.nowTime = 0;
+//-     //   this.diffTime = 0;
+//-     //   this.times = [];
+//-     //   this.stopTimer();
+//-     //   this.animateFrame = 0;
+//-     // }
+//-   },
+//-   computed: {
+//-     hours: function () {
+//-       return Math.floor(this.diffTime / 1000 / 60 / 60);
+//-     },
+//-     minutes: function () {
+//-       return Math.floor(this.diffTime / 1000 / 60) % 60;
+//-     },
+//-     seconds: function () {
+//-       return Math.floor(this.diffTime / 1000) % 60;
+//-     },
+//-     milliSeconds: function () {
+//-       return Math.floor(this.diffTime % 1000);
+//-     }
+//-   },
+//-   filters: {
+//-     zeroPad: function(value){
+//-       let num = typeof num !== 'undefined' ? num : 2;
+//-       return value.toString().padStart(num,"0");
+//-     }
+//-   }
+//- });
+//- </script> -->
