0d223ffa0dd412ab25b0c69a8e3ac293837d7a5b
vinayQA <vinay@samitsolutions.com.com>
2018-11-21 21:14:25 +0530

    d

--- src/fields/core/fieldHeader.vue
+++ src/fields/core/fieldHeader.vue
@@ -1,14 +1,14 @@
 <template>
-  <!-- <section class="hero" :class="schema.heroColorClass">
+  <section class="hero" :class="schema.heroColorClass">
     <div :class="schema.heroBodyClass">
-        <!-- <h1 class="title">
+        <h1 class="title">
           {{schema.title}}
         </h1>
         <h2 class="subtitle">
           {{schema.subtitle}}
-        </h2> -->
+        </h2>
     </div>
-  </section> -->
+  </section>
 </template>
 
 <script>
